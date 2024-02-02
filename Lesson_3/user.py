8c0976216af8496859c115f8af122e06ce65302d
AlexeyFef <alexfef72@gmail.com>
2024-02-02 22:37:34 +0100

    Lesson 3

---
+++ Lesson_3/user.py
@@ -0,0 +1,22 @@
+class User:
+    
+    first_name = "No First Name"
+    last_name = "No Lst Name"
+    
+    def __init__(self, _first_name, _last_name):
+        self.first_name = _first_name
+        self.last_name = _last_name        
+    
+    def sayFirstName(self):
+        print(self.first_name)
+
+    def sayLastName(self):
+        print(self.last_name)
+
+    def sayFirstNameLastName(self):
+        print(self.first_name, self.last_name)    
+
+newUser = User('Alex', 'Fef')
+newUser.sayFirstName()
+newUser.sayLastName()
+newUser.sayFirstNameLastName()
