f5422a53c3f92fb6cdc61cfda0b65438873adb7f
AlexeyFef <alexfef72@gmail.com>
2024-02-06 23:08:04 +0100

    Lesson 3 task 3

---
+++ Lesson_3/lesson_3_task_3.py
@@ -0,0 +1,11 @@
+from address import Address
+from mailing import Mailing
+
+new_mailing = Mailing(Address('123456', 'Москва', 'Ленина', '100', '112'), 
+                      Address('352896', 'Казань', 'Попова', '22', '1'),
+                      1000, '0125895-254')
+
+print("Отправление № ", new_mailing.track, " из ", new_mailing.from_address.address_index,", город ", new_mailing.from_address.address_city, ", улица ",
+new_mailing.from_address.address_street, ", дом/квартира ", new_mailing.from_address.address_buildind, "-", new_mailing.from_address.address_flat, 
+", в ", new_mailing.to_address.address_index, ", город ", new_mailing.to_address.address_city, ", улица ", new_mailing.to_address.address_street, ", дом/квартира ",
+new_mailing.to_address.address_buildind, "-", new_mailing.to_address.address_flat, ". Стоимость ", new_mailing.cost, " рублей.", sep = '')
