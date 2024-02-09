388a417a7ba057a062296fc271d615c2db6e0779
Alexey Fefelov <alexfef72@gmail.com>
2024-02-10 00:11:45 +0100

    Update user.py

--- Lesson_3/user.py
+++ Lesson_3/user.py
@@ -1,19 +1,16 @@
 # создаем класс User, объявляем конструктор, создаем три класса 
 
 class User:
-
-    first_name = "No First Name"
-    last_name = "No Lst Name"
     
     def __init__(self, _first_name, _last_name):
         self.first_name = _first_name
         self.last_name = _last_name        
     
-    def sayFirstName(self):
+    def say_first_name(self):
         print(self.first_name)
 
-    def sayLastName(self):
+    def say_last_name(self):
         print(self.last_name)
 
-    def sayFirstNameLastName(self):
+    def say_first_name_last_name(self):
         print(self.first_name, self.last_name)    
