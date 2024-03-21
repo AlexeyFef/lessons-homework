46b6a5ff641c505b8c5c3f941c6a243054bb89b2
AlexeyFef <alexfef72@gmail.com>
2024-03-21 22:54:57 +0100

    new testt

--- Lesson_4/test_string_utils.py
+++ Lesson_4/test_string_utils.py
@@ -2,52 +2,52 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
-def test_all_lowercase_letters():
+def test_capitilize_all_lowercase_letters():
     string_utils = StringUtils()
     res = string_utils.capitilize("my first test")
     assert res == "My first test"
 
-def test_all_lowercase_letters_except_first():
+def test_capitilize_all_lowercase_letters_except_first():
     string_utils = StringUtils()
     res = string_utils.capitilize("My first test")
     assert res == "My first test"   
 
-def test_empty_string():
+def test_capitilize_empty_string():
     string_utils = StringUtils()
     res = string_utils.capitilize("")
     assert res == ""
 
-def test_only_spaces():
+def test_capitilize_only_spaces():
     string_utils = StringUtils()
     res = string_utils.capitilize("   ")
     assert res == "   "
 
-def test_only_numbers():
+def test_capitilize_only_numbers():
     string_utils = StringUtils()
     res = string_utils.capitilize("12345")
     assert res == "12345"
 
-def test_without_argument():
+def test_capitilize_without_argument():
     string_utils = StringUtils()
     res = string_utils.capitilize()
     assert res == "   "             
 
-def test_all_lowercase_letters_except_first_number():
+def test_capitilize_all_lowercase_letters_except_first_number():
     string_utils = StringUtils()
     res = string_utils.capitilize("5my first test")
     assert res == "5My first test"
 
-def test_all_capitals_letters_except_first_lowercase():
+def test_capitilize_all_capitals_letters_except_first_lowercase():
     string_utils = StringUtils()
     res = string_utils.capitilize("sKYPRO")
     assert res == "SKYPRO"
 
-def test_spaces_before():
+def test_trim_spaces_before():
     string_utils = StringUtils()
     res = string_utils.trim("      My first test")
     assert res == "My first test"
 
-def test_without_spaces_before():
+def test_trim_without_spaces_before():
     string_utils = StringUtils()
     res = string_utils.trim("My first test")
     assert res == "My first test"
@@ -56,3 +56,23 @@ def test_string_to_list():
     string_utils = StringUtils()
     res = string_utils.to_list("My,first,test")
     assert res == ["My", "first", "test"]
+
+def test_string_with_numbers_to_list():
+    string_utils = StringUtils()
+    res = string_utils.to_list("My,first,test99,7,35,156")
+    assert res == ["My", "first", "test99", "7", "35", "156"]
+
+def test_string_with_numbers_and_delimeter_to_list():
+    string_utils = StringUtils()
+    res = string_utils.to_list("My;first;test99;7;35;156", ";")
+    assert res == ["My", "first", "test99", "7", "35", "156"]
+
+def test_string_to_list_empty_string():
+    string_utils = StringUtils()
+    res = string_utils.to_list("")
+    assert res == []
+
+def test_string_to_list_one_symbol():
+    string_utils = StringUtils()
+    res = string_utils.to_list("M")
+    assert res == ["M"]
