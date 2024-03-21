8a937c5f81f6399a2bba8be5d71b6e715dc9e268
AlexeyFef <alexfef72@gmail.com>
2024-03-21 16:18:22 +0100

    New test Pytest

--- Lesson_4/test_string_utils.py
+++ Lesson_4/test_string_utils.py
@@ -12,12 +12,47 @@ def test_all_lowercase_letters_except_first():
     res = string_utils.capitilize("My first test")
     assert res == "My first test"   
 
+def test_empty_string():
+    string_utils = StringUtils()
+    res = string_utils.capitilize("")
+    assert res == ""
+
+def test_only_spaces():
+    string_utils = StringUtils()
+    res = string_utils.capitilize("   ")
+    assert res == "   "
+
+def test_only_numbers():
+    string_utils = StringUtils()
+    res = string_utils.capitilize("12345")
+    assert res == "12345"
+
+def test_without_argument():
+    string_utils = StringUtils()
+    res = string_utils.capitilize()
+    assert res == "   "             
+
 def test_all_lowercase_letters_except_first_number():
     string_utils = StringUtils()
-    res = string_utils.capitilize("9my first test")
-    assert res == "9My first test"    
+    res = string_utils.capitilize("5my first test")
+    assert res == "5My first test"
 
 def test_all_capitals_letters_except_first_lowercase():
     string_utils = StringUtils()
-    res = string_utils.capitilize("mY FIRST TEST")
-    assert res == "MY FIRST TEST"
+    res = string_utils.capitilize("sKYPRO")
+    assert res == "SKYPRO"
+
+def test_spaces_before():
+    string_utils = StringUtils()
+    res = string_utils.trim("      My first test")
+    assert res == "My first test"
+
+def test_without_spaces_before():
+    string_utils = StringUtils()
+    res = string_utils.trim("My first test")
+    assert res == "My first test"
+
+def test_string_to_list():
+    string_utils = StringUtils()
+    res = string_utils.to_list("My,first,test")
+    assert res == ["My", "first", "test"]
